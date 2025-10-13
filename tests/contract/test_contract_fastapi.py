a3def50c23593007280f8f290c2a721f69583891
jaydeep <jaydeepk@gmail.com>
2025-10-13 22:29:04 +0530

    Passed stub data dir path as an argument. Added conftest.py

--- tests/contract/test_contract_fastapi.py
+++ tests/contract/test_contract_fastapi.py
@@ -1,4 +1,5 @@
 import os
+
 import pytest
 
 from specmatic.core.specmatic import Specmatic
@@ -8,10 +9,12 @@ from app import app as fastapi_app
 
 APP_HOST = "127.0.0.1"
 APP_PORT = 8000
+
 STUB_HOST = "127.0.0.1"
 STUB_PORT = 8080
+STUB_DATA_DIR = ROOT_DIR + "tests/data"
+
 SPECMATIC_CONFIG_FILE_PATH = ROOT_DIR + '/specmatic.yaml'
-expectation_json_file = ROOT_DIR + '/tests/data/stub_products_200.json'
 
 os.environ["SPECMATIC_GENERATIVE_TESTS"] = "true"
 
@@ -22,10 +25,12 @@ class TestContract:
 
 (
     Specmatic()
-    .with_specmatic_config_file_path(SPECMATIC_CONFIG_FILE_PATH)
-    .with_project_root(ROOT_DIR)
+    .with_specmatic_config_file_path(str(SPECMATIC_CONFIG_FILE_PATH))
+    .with_project_root(str(ROOT_DIR))
     .with_stub(
-        STUB_HOST, STUB_PORT, [expectation_json_file]
+        STUB_HOST,
+        STUB_PORT,
+        args=[f"--data={STUB_DATA_DIR}"]
     ).with_asgi_app('app:app', APP_HOST, APP_PORT)
     .test_with_api_coverage_for_fastapi_app(
         TestContract, fastapi_app
